1f345456e23a85c34d9e58d4c452768db7318531
Alice Won <alicewon4@gmail.com>
2020-11-01 23:27:12 -0800

    basic types

---
+++ index.ts
@@ -0,0 +1,24 @@
+const isOpen: boolean = false
+
+const myName: string = 'alice'
+
+const age: number = 30
+
+const list: number[] = [0,1,2,3]
+
+// tuple: has to follow order of types declared
+const me: [string, number] = ["Alice", 30]
+
+// enum
+enum Job { WebDev, WebDesigner, PM }
+const job: Job = Job.WebDev
+
+// any
+const phone: any = "Pixel"
+const tablet: any = 3
+
+// never
+// undefined
+// null
+
+
